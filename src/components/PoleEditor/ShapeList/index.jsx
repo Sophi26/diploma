b6facd9044bdb4e6518705db5be913df2a50e2c5
Sophi26 <35414039+Sophi26@users.noreply.github.com>
2018-05-27 23:30:28 +0500

    Drag n Drop

--- src/components/PoleEditor/ShapeList/index.jsx
+++ src/components/PoleEditor/ShapeList/index.jsx
@@ -22,8 +22,8 @@ class ShapeList extends React.Component {
             const view = figure.icon.attrs.viewBox.split(' ');
             const x = Number(view[0]) + Number(view[2]) / 2;
             const y = Number(view[1]) + Number(view[3]) / 2;
-            return <div className="draggable" key={key}>
-                        <svg width={figure.icon.attrs.width} height={figure.icon.attrs.height} viewBox={figure.icon.attrs.viewBox}>
+            return <div className="shape-flex-box" key={key}>
+                        <svg className="draggable" width={figure.icon.attrs.width} height={figure.icon.attrs.height} viewBox={figure.icon.attrs.viewBox}>
                             <path fill={figure.icon.childs[0].attrs.fill} d={figure.icon.childs[0].attrs.d} />
                             <text x={x} y={y} alignmentBaseline="middle" textAnchor="middle">{figure.concept !== undefined ? figure.concept : ''}</text>
                         </svg>
@@ -40,28 +40,25 @@ class ShapeList extends React.Component {
     initDrag(e) {
 
         console.log("START!!!");
-
-        console.log(e.which);
-
-        if (e.which != 1) {
-            return;
-        }
         
         let elem = e.target.closest('.draggable');
         console.log(elem);
         if (!elem) return;
         
-        this.setState({
-            dragObject: {
-                elem: elem,
-                downX: e.pageX,
-                downY: e.pageY,
-            },
-        });
+        let drag_tmp = {
+            elem: elem,
+            downX: e.pageX,
+            downY: e.pageY,
+        };
+
+        this.setState({ dragObject: drag_tmp });
     }
 
     moveDrag(e) {
 
+        console.log("MOVE!!!");
+        console.log(this.state);
+        
         if (this.state.dragObject.elem === undefined) return;
 
         if (this.state.dragObject.avatar === undefined) {
@@ -69,76 +66,77 @@ class ShapeList extends React.Component {
             let moveX = e.pageX - this.state.dragObject.downX;
             let moveY = e.pageY - this.state.dragObject.downY;
             if (Math.abs(moveX) < 3 && Math.abs(moveY) < 3) return;
+            let avatar = this.state.dragObject.elem;
+            
+            console.log("AVATAR!!!");
+            console.log(avatar);
 
-            this.setState({ dragObject: Object.assign(this.state.dragObject, {avatar: this.createAvatar.bind(this, e)}), });
-            if (this.state.dragObject.avatar === undefined) {
-                this.setState({ dragObject: {} });
-                return;
-            }
+            /*let old = {
+                parent: avatar.parentNode,
+                nextSibling: avatar.nextSibling,
+                position: avatar.style.position || '',
+                left: avatar.style.left || '',
+                top: avatar.style.top || '',
+                zIndex: avatar.style.zIndex || ''
+            };
+            avatar.rollback = () => {
+                old.parent.insertBefore(avatar, old.nextSibling);
+                avatar.style.position = old.position;
+                avatar.style.left = old.left;
+                avatar.style.top = old.top;
+                avatar.style.zIndex = old.zIndex;
+            };*/
 
-            let coords = getCoords(this.state.dragObject.avatar);
-            let shiftX = this.state.dragObject.downX - coords.left;
-            let shiftY = this.state.dragObject.downY - coords.top;
-            this.setState({ dragObject: Object.assign(this.state.dragObject, {shiftX: shiftX, shiftY: shiftY}), });
+            this.setState({ dragObject: Object.assign(this.state.dragObject, {avatar: avatar}) }, () => {
 
-            this.startDrag.bind(this, e);
+                if (this.state.dragObject.avatar === undefined) {
+                    this.setState({ dragObject: {} });
+                    return;
+                }
+    
+                let box = this.state.dragObject.avatar.getBoundingClientRect();      
+                let coords = {
+                    top: box.top + pageYOffset,
+                    left: box.left + pageXOffset,
+                };
+
+                console.log("COORDS!!!");
+                console.log(coords);
+
+                let shiftX = this.state.dragObject.downX - coords.left;
+                let shiftY = this.state.dragObject.downY - coords.top;
+                this.setState({ dragObject: Object.assign(this.state.dragObject, {shiftX: shiftX, shiftY: shiftY}) }, () => {
+      
+                    document.body.appendChild(this.state.dragObject.avatar);
+                    this.state.dragObject.avatar.style.zIndex = 9999;
+                    this.state.dragObject.avatar.style.position = 'absolute';
+                    this.state.dragObject.avatar.style.left = e.pageX - this.state.dragObject.shiftX + 'px';
+                    this.state.dragObject.avatar.style.top = e.pageY - this.state.dragObject.shiftY + 'px';
+                });  
+            });  
+        } else {
+
+            this.state.dragObject.avatar.style.left = e.pageX - this.state.dragObject.shiftX + 'px';
+            this.state.dragObject.avatar.style.top = e.pageY - this.state.dragObject.shiftY + 'px';
         }
-
-        this.state.dragObject.avatar.style.left = e.pageX - this.state.dragObject.shiftX + 'px';
-        this.state.dragObject.avatar.style.top = e.pageY - this.state.dragObject.shiftY + 'px';
-    }
-
-    createAvatar(e) {
-
-        let avatar = this.state.dragObject.elem;
-        let old = {
-          parent: avatar.parentNode,
-          nextSibling: avatar.nextSibling,
-          position: avatar.position || '',
-          left: avatar.left || '',
-          top: avatar.top || '',
-          zIndex: avatar.zIndex || ''
-        };
-      
-        avatar.rollback = () => {
-          old.parent.insertBefore(avatar, old.nextSibling);
-          avatar.style.position = old.position;
-          avatar.style.left = old.left;
-          avatar.style.top = old.top;
-          avatar.style.zIndex = old.zIndex;
-        };
-      
-        return avatar;
-    }
-
-    startDrag(e) {
-
-        let avatar = this.state.dragObject.avatar;
-      
-        document.body.appendChild(avatar);
-        avatar.style.zIndex = 9999;
-        avatar.style.position = 'absolute';
     }
 
     endDrag(e) {
 
+        console.log("END!!!");
+
         if(this.state.dragObject.avatar !== undefined) {
-            this.finishDrag.bind(this, e);
+
+            this.state.dragObject.avatar.hidden = true;
+            let elem = document.elementFromPoint(e.clientX, e.clientY);
+            this.state.dragObject.avatar.hidden = false;
+        
+            if (elem == null) return null;
+            
+            console.log(elem.closest('.droppable'));
         }
 
         this.setState({ dragObject: {} });
-    }
-
-    findDroppable(e) {
-
-        this.state.dragObject.avatar.hidden = true;
-        let elem = document.elementFromPoint(e.clientX, e.clientY);
-        this.state.dragObject.avatar.hidden = false;
-      
-        if (elem == null) return null;
-        
-        console.log("END!!!");
-        console.log(elem.closest('.droppable'));
     }
 }
 
