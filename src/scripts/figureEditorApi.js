ca694de9e39e6de4cb85c8999060e22cecd4c14e
Sophi26 <35414039+Sophi26@users.noreply.github.com>
2018-06-20 16:53:28 +0500

    Syntax Error

--- src/scripts/figureEditorApi.js
+++ src/scripts/figureEditorApi.js
@@ -12,47 +12,34 @@ import { onlyImportantFeatures } from '../actions/FigureEditorActions';
 
 function main(store) {
 
-    render( < Provider store = { store } >
-            <
-            FigureList / >
-            <
-            /Provider>, document.getElementById("shape-ul"));
+    render( <Provider store={store}><FigureList /></Provider>, document.getElementById("shape-ul"));
 
-            render( < Provider store = { store } >
-                <
-                FigureInfo / >
-                <
-                /Provider>, document.getElementById("shape-description"));
+    render( <Provider store={store}><FigureInfo /></Provider>, document.getElementById("shape-description"));
 
-                render( < Provider store = { store } >
-                    <
-                    FigureImg / >
-                    <
-                    /Provider>, document.getElementById("select-shape"));
+    render( <Provider store={store}><FigureImg /></Provider>, document.getElementById("select-shape"));
 
-                    let figId = 1;
-                    const input = document.getElementById("upload"); input.addEventListener('change', () => {
-                        const file_name = input.files[0].name.substr(0, input.files[0].name.length - 4);
-                        let data = new FormData();
-                        data.append('file', input.files[0]);
-                        fetch("/api/upload", {
-                                method: "POST",
-                                body: data,
-                            })
-                            .then((response) => {
-                                return response.json();
-                            })
-                            .then((result) => {
-                                const action = addFigure(figId, result, file_name);
-                                store.dispatch(action);
-                                ++figId;
-                            })
-                            .catch();
-                    });
+    let figId = 1;
+    const input = document.getElementById("upload"); input.addEventListener('change', () => {
+        const file_name = input.files[0].name.substr(0, input.files[0].name.length - 4);
+        let data = new FormData();
+        data.append('file', input.files[0]);
+        fetch("/api/upload", {
+            method: "POST",
+            body: data,
+        })
+        .then((response) => {
+            return response.json();
+        })
+        .then((result) => {
+            const action = addFigure(figId, result, file_name);
+            store.dispatch(action);
+            ++figId;
+        })
+        .catch();
+    });
 
-                }
+}
 
-                export {
-                    main as
-                    default,
-                };
+export {
+    main as default,
+};
