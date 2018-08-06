cb19f2572890a8d948be45c07758824cd29d077c
Sophi26 <35414039+Sophi26@users.noreply.github.com>
2018-08-06 23:40:26 +0500

    SaveExperiment

--- src/scripts/createExperiment.js
+++ src/scripts/createExperiment.js
@@ -1,8 +1,13 @@
+import FileSaver from 'file-saver';
+
 function main() {
 
     const create = document.getElementById("create-experiment-flex-block");
 
     create.addEventListener('click', () => {
+
+        let blob = new Blob(["Hello, world!"], {type: "text/plain;charset=utf-8"});
+        FileSaver.saveAs(blob, "hello_world.txt");
 
         document.getElementById("start").style.display = 'none';
         document.getElementById("creation").style.display = 'block';
