b7898d571686fca49e5b440f22e9505fe54e4e1f
Sophi26 <35414039+Sophi26@users.noreply.github.com>
2018-08-15 01:22:40 +0500

    StartWindow (Style)

--- src/scripts/earlyExperiments.js
+++ src/scripts/earlyExperiments.js
@@ -17,9 +17,11 @@ function main() {
 
                 let expFlexBlock = document.createElement("div");
                 expFlexBlock.setAttribute('class', 'experiment');
+                let nameBlock = document.createElement("div");
+                nameBlock.setAttribute('class', 'name-block');
                 let expName = document.createElement("p");
                 expName.appendChild(document.createTextNode(result[i].slice(0, -4)));
-                expFlexBlock.appendChild(expName);
+                nameBlock.appendChild(expName);
                 let menuIcon = document.createElementNS("http://www.w3.org/2000/svg", "svg");
                 menuIcon.setAttribute('width', '34px');
                 menuIcon.setAttribute('height', '34px');
@@ -28,7 +30,8 @@ function main() {
                 path.setAttribute('fill', 'rgba(3, 3, 33, .7)');
                 path.setAttribute('d', 'M12,16A2,2 0 0,1 14,18A2,2 0 0,1 12,20A2,2 0 0,1 10,18A2,2 0 0,1 12,16M12,10A2,2 0 0,1 14,12A2,2 0 0,1 12,14A2,2 0 0,1 10,12A2,2 0 0,1 12,10M12,4A2,2 0 0,1 14,6A2,2 0 0,1 12,8A2,2 0 0,1 10,6A2,2 0 0,1 12,4Z');
                 menuIcon.appendChild(path);
-                expFlexBlock.appendChild(menuIcon);
+                nameBlock.appendChild(menuIcon);
+                expFlexBlock.appendChild(nameBlock);
 
                 expFlexContainer.insertBefore(expFlexBlock, expFlexContainer.firstChild);
             }
